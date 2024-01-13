988d2b39090b9292952825441c296ccd38a015f1
hinogi <stefan.schneider@gmx.net>
2024-01-13 17:42:25 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "vite";
+// import { effectPlugin } from "@effect/vite-plugin-react";
+
+export default defineConfig({
+  plugins: [
+    // effectPlugin({
+    //   babel: {
+    //     plugins: ["babel-plugin-annotate-pure-calls"],
+    //   },
+    // }),
+  ],
+});
